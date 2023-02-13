0dd16efd108713733a50823c2d2b97ed8ad95c85
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2023-02-13 11:53:29 +0900

    useStaff 커스텀 훅

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -4,7 +4,6 @@ import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 
-// for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
   const { data } = await axiosInstance.get('/treatments');
   return data;
