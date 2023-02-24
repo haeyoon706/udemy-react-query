588cc761981fd16372282c8bc4a5e5ef3117a311
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2023-02-24 22:32:42 +0900

    쿼리 취소 가능하게 만들기

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,13 +1,16 @@
-import { useMutation, UseMutateFunction } from 'react-query';
+import {
+  useMutation,
+  UseMutateFunction,
+  QueryClient,
+  useQueryClient,
+} from 'react-query';
 import jsonpatch from 'fast-json-patch';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { useUser } from './useUser';
-import {
-  useCustomToast,
-  userCustomToast,
-} from '../../app/hooks/useCustomToast';
+import { useCustomToast } from '../../app/hooks/useCustomToast';
+import { queryKeys } from 'react-query/constants';
 
 // for when we need a server function
 async function patchUserOnServer(
@@ -38,10 +41,36 @@ export function usePatchUser(): UseMutateFunction<
 > {
   const { user, updateUser } = useUser();
   const toast = useCustomToast();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation(
     (newUserData: User) => patchUserOnServer(newUserData, user),
     {
+      // onMutate returns context that is passed to onError
+      onMutate: async (newData: User | null) => {
+        // cancel any outgoing queries for user data, so old server data
+        // doesn't overwrite our optimistic update
+        queryClient.cancelQueries(queryKeys.user);
+
+        // snpashot of previous user value
+        const previousUserData: User = queryClient.getQueryData(queryKeys.user);
+
+        // optimistically update the cache with new user value
+        updateUser(newData);
+
+        // return context objet with snapshotted value
+        return { previousUserData };
+      },
+      onError: (error, newData, context) => {
+        // roll back cache to saved value
+        if (context.previousUserData) {
+          updateUser(context.previousUserData);
+          toast({
+            title: 'Update failed; restoring previous values',
+            status: 'warning',
+          });
+        }
+      },
       onSuccess: (userData: User | null) => {
         if (user) {
           updateUser(userData);
@@ -50,6 +79,10 @@ export function usePatchUser(): UseMutateFunction<
             status: 'success',
           });
         }
+      },
+      onSettled: () => {
+        // invalidate user query to make sure we're in sync with setver data
+        queryClient.invalidateQueries(queryKeys.user);
       },
     },
   );
