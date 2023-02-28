65675552290c31a9f3e713f3715a8353ddd0a27f
developer-heyy <121794924+haeyoon706@users.noreply.github.com>
2023-03-01 02:39:13 +0900

    필터 테스트

--- base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,19 @@
 import { useStaff } from '../hooks/useStaff';
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { createQueryClientWrapper } from '../../../test-utils';
 
 test('filter staff', async () => {
-  // the magic happens here
+  const { result, waitFor } = renderHook(useStaff, {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for the staff populate
+  await waitFor(() => result.current.staff.length === 4);
+
+  // set to filter for only staff who give massage
+  act(() => result.current.setFilter('facial'));
+
+  // wait for the staff list to display only 3
+  await waitFor(() => result.current.staff.length === 3);
 });
